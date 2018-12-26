786d22a7e66795ce202ee12e2f8e9d6ebd50587f
claudehotline@hotmail.com <claudehotline@hotmail.com>
2018-12-26 22:43:09 +0800

    update

--- app/application.py
+++ app/application.py
@@ -30,7 +30,7 @@ engine = create_engine('sqlite:///data/DisasterResponse.db')
 df = pd.read_sql_table('ResponseCategory', engine)
 
 # load model
-model = joblib.load("models/classifier.pkl")
+model = joblib.load("models/classifier2.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
