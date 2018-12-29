62d1e851e035af795bb96f026222ee4ebd2e776e
Chai Sen <claudehotline@hotmail.com>
2018-12-30 00:35:15 +0800

    update

--- app/application.py
+++ app/application.py
@@ -9,7 +9,7 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-#from sklearn.externals import joblib
+from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
 
@@ -30,10 +30,7 @@ def tokenize(text):
 engine = create_engine('sqlite:///data/DisasterResponse.db')
 df = pd.read_sql_table('ResponseCategory', engine)
 
-# load model
-with open("models/classifier.pkl", 'rb') as f:
-        model = pickle.load(f)
-# model = joblib.load("models/classifier.pkl")
+model = joblib.load("models/classifier.pkl")
 
 # index webpage displays cool visuals and receives user input text for model
 @application.route('/')
